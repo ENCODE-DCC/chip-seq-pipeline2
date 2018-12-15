ff2c187b126058e6c694fe5044b6e187bbd89486
Jin Lee <leepc12@gmail.com>
2018-12-15 00:46:12 -0800

    limit num_threads for task-level tests

--- test/test_task/test.sh
+++ test/test_task/test.sh
@@ -14,6 +14,11 @@ if [ $# -gt 2 ]; then
 else
   DOCKER_IMAGE=quay.io/encode-dcc/chip-seq-pipeline:v1.1.4
 fi
+if [ $# -gt 3 ]; then
+  NUM_TASK=$4
+else
+  NUM_TASK=1
+fi
 
 if [ -f "cromwell-34.jar" ]; then
   echo "Skip downloading cromwell."
@@ -23,6 +28,7 @@ fi
 CROMWELL_JAR=cromwell-34.jar
 BACKEND_CONF=../../backends/backend.conf
 BACKEND=Local
+EXTRA_PARAM="-Dbackend.providers.Local.config.concurrent-job-limit=2"
 PREFIX=$(basename ${WDL} .wdl)
 METADATA=${PREFIX}.metadata.json # metadata
 RESULT=${PREFIX}.result.json # output
@@ -41,7 +47,7 @@ if [ $DOCKER_IMAGE == 'conda' ]; then
 else
   WF_OPT="-o ${TMP_WF_OPT}"
 fi
-java -Dconfig.file=${BACKEND_CONF} -Dbackend.default=${BACKEND} -jar ${CROMWELL_JAR} run ${WDL} -i ${INPUT} ${WF_OPT} -m ${METADATA}
+java -Dconfig.file=${BACKEND_CONF} -Dbackend.default=${BACKEND} ${EXTRA_PARAM} -jar ${CROMWELL_JAR} run ${WDL} -i ${INPUT} ${WF_OPT} -m ${METADATA}
 
 # parse output metadata json
 cat ${METADATA} | python -c "import json,sys;obj=json.load(sys.stdin);print(obj['outputs']['${PREFIX}.compare_md5sum.json_str'])" > ${RESULT}
