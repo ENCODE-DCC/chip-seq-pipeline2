556c8d63ed69e19f6943bf8f0915a8cc3e034335
Jin wook Lee <leepc12@gmail.com>
2021-10-21 20:14:36 -0700

    fix conda installer

--- scripts/install_conda_env.sh
+++ scripts/install_conda_env.sh
@@ -4,11 +4,11 @@ set -e  # Stop on error
 SH_SCRIPT_DIR=$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)
 
 echo "** Installing pipeline's Conda environments..."
-conda create -n encode-chip-seq-pipeline --file requirements.txt \
+conda create -n encode-chip-seq-pipeline --file ${SH_SCRIPT_DIR}/requirements.txt \
   --override-channels -c bioconda -c defaults -y
-conda create -n encode-chip-seq-pipeline-macs2 --file requirements.macs2.txt \
+conda create -n encode-chip-seq-pipeline-macs2 --file ${SH_SCRIPT_DIR}/requirements.macs2.txt \
   --override-channels -c bioconda -c defaults -y
-conda create -n encode-chip-seq-pipeline-spp --file requirements.spp.txt \
+conda create -n encode-chip-seq-pipeline-spp --file ${SH_SCRIPT_DIR}/requirements.spp.txt \
   --override-channels -c r -c bioconda -c defaults -y
 echo "** Done."
 
